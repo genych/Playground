47b927b4a731cab35ca339acda62bcc975163482
genych <evgeny.she+git@gmail.com>
2015-09-02 14:39:28 +0300

    ok?

--- snippets/sql_stuff.py
+++ snippets/sql_stuff.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import sqlite3
 import time
 
@@ -24,30 +23,3 @@ db = base.cursor()
 # db.execute('''CREATE TABLE Parts (id text, quantity int)''')
 
 main() 
-=======
-import sqlite3
-import time
-
-def prompt():
-    prod_id = raw_input('id ')
-    quantity = int(raw_input('quantity '))
-    return unicode(time.asctime()), prod_id, quantity
-
-
-def main():
-    while True:
-        db.execute('INSERT INTO Product VALUES (?, ?, ?)', (prompt()))
-        base.commit()
-        again = raw_input('another one? ')
-        if not again:
-            break
-    base.close()
-
-
-base = sqlite3.connect('../static/database.sqlite')
-db = base.cursor()
-# db.execute('CREATE TABLE Product (time text, id text, quantity int)')
-# db.execute('''CREATE TABLE Parts (id text, quantity int)''')
-
-main() 
->>>>>>> 5d67aefaf7fb349cc4275cc67b614f6e10122b35
--- snippets/xmlstuff.py
+++ snippets/xmlstuff.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from bs4 import BeautifulSoup
 
 result = []
@@ -13,19 +12,3 @@ for child in children:
                     'location': child.parent.get('location'),
                     'type': child.get('type'),
                     'serial': child.get('serial')})
-=======
-from bs4 import BeautifulSoup
-
-result = []
-
-with open('../static/dev.xml') as file:
-    xml = BeautifulSoup(file)
-
-children = xml.find_all('optic')
-
-for child in children:
-    result.append({'id': child.parent.get('id'), 
-                    'location': child.parent.get('location'),
-                    'type': child.get('type'),
-                    'serial': child.get('serial')})
->>>>>>> 5d67aefaf7fb349cc4275cc67b614f6e10122b35
